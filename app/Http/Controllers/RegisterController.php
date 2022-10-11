476540b3f662b24805f7137b1fe21871f4281cb6
c2run <jose.cerda66@gmail.com>
2022-10-11 17:38:08 -0300

    Se añade la clase auth y el método check al controlador RegisterController para validar si el usuario está logueado no le permita ir al formulario de registro y lo redirija al home

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,10 +5,14 @@ namespace App\Http\Controllers;
 use App\Http\Requests\registerRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
     public function show(){
+        if(Auth::check()){
+            return redirect('/home');
+        }
         return view('auth.register');
     }
     public function register(registerRequest $request){
